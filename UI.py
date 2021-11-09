e6724b6232c5c4e57a4bad72edc002319b18aa2a
NowlasWolf <38125683+NowlasWolf@users.noreply.github.com>
2021-11-09 14:31:44 -0800

    Functional version of UI.py
    UI.py now can select options and send the command through os.system

--- UI.py
+++ UI.py
@@ -1,10 +1,10 @@
 import tkinter as tk
+import os
 
-commands = {}
-
-def parsetool():
+def parsetool(tn):
+    commands = {}
     try:
-        fin = open("tool.py","r")
+        fin = open(tn,"r")
     except:
         print("Tool File Open Error")
         quit()
@@ -21,43 +21,73 @@ def parsetool():
                     commands[line.split(" ")[1].split("(")[0]] = options
             line = fin.readline()
         fin.close()
-        print (commands)
+        return commands
 
-def submitcommand():
-    print("Submit Clicked")
 
-def changeoptions(var, index, mode):
-    print("Option Change")
-    print(window.getvar(var))
+class EZUI:
 
-def drawwin():
-    parsetool()
-    window = tk.Tk()
-    window.iconbitmap("icon.ico")
-    window.title("Translation Tool EZUI")
+    def submitcommand(self):
+        print("Submit Clicked")
+        endstring = ""
+        for i, x in enumerate(self.optionselected):
+            if x.get() == 1:
+                endstring = endstring + " " + self.commands[self.v.get()][i]
+        endstring = self.v.get() + endstring
+        print("executing: " + endstring)
+        os.system("cmd /k tool.exe " + endstring)
+
+    def changeoptions(self,*args):
+        self.optionselected = []
+        print("Option Changed to " + self.v.get())
+        for i in self.optionselects:
+            i.destroy()
+        self.optionselects = []
+        count = 1
+        for i in self.commands[self.v.get()]:
+            newvar = tk.IntVar()
+            newcb = tk.Checkbutton(self.window, text=i, variable=newvar)
+            newcb.grid(row=count, column=1)
+            self.optionselects.append(newcb)
+            self.optionselected.append(newvar)
+            count += 1
+            
+        self.submit.grid(row=count,columnspan=2)
+        self.label.grid(row=0, column=0, padx=0, rowspan=(count-1>0 or 1))
     
-    topframe = tk.Frame(master=window, width=50).pack(side=tk.TOP)
-    frame1 = tk.Frame(master=topframe, width=25).pack(side=tk.LEFT, fill=tk.BOTH)
-    label = tk.Label(master = frame1,
+    def __init__(self,toolname):
+        self.window = tk.Tk()
+        self.window.iconbitmap("icon.ico")
+        self.window.title("Translation Tool EZUI")
+        
+
+        self.label = tk.Label(self.window,
         text="Pick an operation and any flags you may want",
         fg="black",
         bg="white",
-        height=25
-    ).pack(side=tk.LEFT, fill=tk.BOTH)
-    frame2 = tk.Frame(master=topframe, width=25).pack(side=tk.LEFT, fill=tk.BOTH)
-    v = tk.StringVar()
-    x = 0
-    optionmenu = tk.OptionMenu(frame2, v, *list(commands)).pack(fill=tk.BOTH)
-    bottomframe = tk.Frame(master=window, width=50).pack(side=tk.TOP,fill=tk.BOTH)
-    submit = tk.Button(bottomframe,width=50,text="Submit", command=submitcommand).pack(fill=tk.BOTH)
+        width=25,
+        height=5,
+        wraplength=100,
+        justify = tk.CENTER
+        )
+        self.label.grid(row=0, column=0, padx=0, rowspan=2)
+        self.submit = tk.Button(self.window,width=50,text="Submit", command=self.submitcommand)
+        self.submit.grid(row=2,columnspan=2)
+        
+        self.commands = parsetool(toolname)
+        self.v = tk.StringVar()
+        self.v.set(list(self.commands)[0])
+        self.v.trace("w", self.changeoptions)
+        self.commandmenu = tk.OptionMenu(self.window, self.v,*list(self.commands)).grid(row=0, column=1)
+
+        self.optionselected = []
+        self.optionselects = []
+        self.changeoptions()
+        
+        
+        self.window.mainloop()
+        
     
-
-    v.trace("w", changeoptions)
-    
-    window.mainloop()
-
-
-
     
 if __name__ == "__main__":
-    drawwin()
+    thewin = EZUI("tool.py")
+    #drawwin()
